3dcaf66f47151e78742093252d5bbd49d50c3779
Gabriel Glez <gabrielgg85@gmail.com>
2025-09-13 21:32:05 +0100

    Works . Now, refactor memory map with new sprites

--- main.asm
+++ main.asm
@@ -7,29 +7,20 @@ BasicUpstart2(main)
 *=MAIN_CODE_ADDRESS "Main Code"
 #import "/includes/main_code.asm"
 
-
 *=TILESET_ADDRESS "Tileset"
-//.import binary "/tileset/tileset.bin"
 .import binary "/tileset/cutregametileset.bin"
 
-
 *=CHARSET_ATTRIB_ADDRESS "Charset Attrib"
-//.import binary "/charset/cutregametilesetattr.bin"
 .import binary "/charset/charsetAttrib.bin"
-
 
 *=VARIABLES_ADDRESS "Variables"
 #import "/includes/variables.asm"
 
-
 *=SPRITES_ADDRESS "Sprites"
 .import binary "/sprites/tank_sprite_set.bin"
 
-
 *=CHARSET_ADDRESS "Charset"
 .import binary "/charset/cutregamecharset.bin"
-//.import binary "/charset/charsetcutregame.bin"
-
 
 *=TABLES_ADDRESS "Tables"
 #import "/includes/tables.asm"
@@ -37,11 +28,5 @@ BasicUpstart2(main)
 *=LIBRARIES_ADDRESS "Libraries"
 #import "/includes/libraries.asm"
 
-
 *=MAP_ADDRESS "Maps"
 .import binary "/maps/backgroundmap.bin"
-
-
-//*=MAPS_COLOR_ADDRESS "Maps Colors"
-//.import binary "/maps/colormap.bin"
-
--- system/memory_map.asm
+++ system/memory_map.asm
@@ -3,16 +3,15 @@
 .label SCREEN_RAM              = $0400
 .label SCREEN_COLOR_RAM        = $d800
 .label MAIN_CODE_ADDRESS       = $0810
-.label TILESET_ADDRESS		   = $2000  // $0e00
-//.label MUSIC_ADDRESS		   = $1000
-.label CHARSET_ATTRIB_ADDRESS  = $2400 
-.label VARIABLES_ADDRESS       = $2500
-.label SPRITES_ADDRESS		   = $2800  // $2800
+.label CHARSET_ATTRIB_ADDRESS  = $1000  // $2400 
+.label TILESET_ADDRESS		   = $1100  // $0e00
+.label VARIABLES_ADDRESS       = $1200  // $2500
+.label SPRITES_ADDRESS		   = $2000  // $2800
 .label CHARSET_ADDRESS		   = $3800  // no change
 .label TABLES_ADDRESS		   = $4000
 .label LIBRARIES_ADDRESS	   = $5000
 
 .label MAP_ADDRESS			   =  $9C7C  //$7000
-//.label MAPS_COLOR_ADDRESS		= $8900
+
 
 
