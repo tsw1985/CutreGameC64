f0dcbc66ba9cc33b6b5b879a6ebeef1839a5d9ed
Gabriel Glez <gabrielgg85@gmail.com>
2025-09-20 06:54:00 +0100

    Added check collision RIGHT CHAIN - LEFT Direction

--- libraries/sprite_lib/sprite_lib.asm
+++ libraries/sprite_lib/sprite_lib.asm
@@ -1312,7 +1312,7 @@ push_regs_to_stack()
 
 
     // TODO : PLAYER_1_TANK_1_LEFT_CHAIN_Y
-    ldx #SPRITE_TANK_1
+    /*ldx #SPRITE_TANK_1
     lda sprites_coord_table_y,x
 
     // apply Y offset to point to center of cannon
@@ -1334,9 +1334,32 @@ push_regs_to_stack()
     lsr
     lsr
     sta SCREEN_COL_POS  // ( X position)
-    sta PLAYER_1_TANK_1_LEFT_CHAIN_X
+    sta PLAYER_1_TANK_1_LEFT_CHAIN_X*/
 
     // TODO : PLAYER_1_TANK_1_RIGHT_CHAIN_Y
+    ldx #SPRITE_TANK_1
+    lda sprites_coord_table_y,x
+
+    // apply Y offset to point to center of cannon
+    sec
+    sbc #45
+    lsr
+    lsr
+    lsr
+    sta PLAYER_1_TANK_1_RIGHT_CHAIN_Y
+    sta SCREEN_ROW_POS   // ( Y position)
+
+
+    ldx #SPRITE_TANK_1
+    lda sprites_coord_table_x,x
+    // apply X offset to point to center of cannon
+    sec
+    sbc #18
+    lsr
+    lsr
+    lsr
+    sta SCREEN_COL_POS  // ( X position)
+    sta PLAYER_1_TANK_1_RIGHT_CHAIN_X
 
 
 
