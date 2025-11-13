85ce249d8e69606b428616252841fa9b8c41cdf7
gabrielgg85 <gabrielgg85@gmail.com>
2025-11-13 11:54:26 +0000

    Added disable bullet tank 2 - left direction

--- libraries/sprite_lib/sprite_lib.asm
+++ libraries/sprite_lib/sprite_lib.asm
@@ -1822,6 +1822,7 @@ push_regs_to_stack()
 
     move_bullet_to_left_player_2:
 
+        /*
         lda SPRITE_3_MEM_X // Comprobamos si la bala se sale de los bordes de pantalla
         cmp #40   // si llego al tope, o colisiona con alguna pared ( de momento 
                   // los limites de pantalla) desaparecemos bala player 1
@@ -1831,7 +1832,62 @@ push_regs_to_stack()
         sbc #BULLET_SPEED                         // decrement Y of bullet sprite player 1
         sta SPRITE_3_MEM_X
         jmp exit_move_bullet_tank_2_player_2
+        */
 
+        lda SPRITE_3_MEM_X // Comprobamos si la bala se sale de los bordes de pantalla
+        cmp #40   // si llego al tope, o colisiona con alguna pared ( de momento 
+                  // los limites de pantalla) desaparecemos bala player 1
+
+        bcc bullet_limit_left_player_2               // si la bala llega a 80 px de alto
+        sec
+        sbc #BULLET_SPEED                         // decrement Y of bullet sprite player 1
+        sta SPRITE_3_MEM_X
+
+        // save current Y in sprites coords table
+        ldx #3
+        lda SPRITE_3_MEM_Y
+        
+        sec //sec
+        sbc #40 //10 //sbc #40 // 50
+        lsr
+        lsr
+        lsr
+        sta sprites_coord_table_y,x
+
+        // also save X
+        lda SPRITE_3_MEM_X
+        sec    // offset for bullet
+        sbc #15 //26 *****
+        lsr
+        lsr
+        lsr
+        sta sprites_coord_table_x,x
+
+
+        //load coords
+        ldx #3
+        lda sprites_coord_table_x,x
+        sta SCREEN_COL_POS_SCREEN_CHAR_BULLET_TANK_2
+        
+        lda sprites_coord_table_y,x
+        sta SCREEN_ROW_POS_SCREEN_CHAR_BULLET_TANK_2
+
+
+        //detect if bullet collides with a brik
+        jsr PRINT_LIB.get_char_value_from_video_memory_bullet_tank_2
+
+        lda CURRENT_CHAR_IN_SCREEN_BULLET_TANK_2
+        cmp #67
+        bne go_to_default_left_player_2 
+        //inc $d020 // change border color
+        jmp bullet_limit_left_player_2
+
+        go_to_default_left_player_2:
+            jmp exit_move_bullet_tank_2_player_2
+
+
+
+        /* Normal left */
         bullet_limit_left_player_2:
             
             //finish fire
@@ -1842,6 +1898,11 @@ push_regs_to_stack()
 
             jmp exit_move_bullet_tank_2_player_2
 
+
+
+    /******************/
+    /*     RIGHT      */
+    /******************/
 
     move_bullet_to_right_player_2:
 
